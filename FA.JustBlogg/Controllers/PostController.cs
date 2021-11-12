7c2d2d7416a7ac64694d9cd8cef4dfbd7d2b75ad
vincentdyland <vincentdyland@gmail.com>
2021-11-13 02:12:22 +0700

    P1

---
+++ FA.JustBlogg/Controllers/PostController.cs
@@ -0,0 +1,63 @@
+﻿using FA.JustBlogg.Context;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FA.JustBlogg.Controllers
+{
+    public class PostController : Controller
+    {
+        // GET: Post
+        FA_JustBloggEntities dbObj = new FA_JustBloggEntities();
+        public ActionResult Post(tbl_PostList obj)
+        {
+            return View(obj);
+        }
+        [HttpPost]
+        public ActionResult AddPost(tbl_PostList model)
+        {
+            if (ModelState.IsValid)
+            {
+                tbl_PostList obj = new tbl_PostList();
+                obj.Name = model.Name;
+                obj.ShortDescription = model.ShortDescription;
+                obj.Description = model.Description;
+                obj.PostedOn = model.PostedOn;
+
+                if(model.ID == 0)
+                {
+                    dbObj.tbl_PostList.Add(obj);
+                    dbObj.SaveChanges();
+                }
+                else
+                {
+                    dbObj.Entry(obj).State = EntityState.Modified;
+                    dbObj.SaveChanges();
+                }
+            }
+            ModelState.Clear();
+                
+            
+            return View("Post");
+        }
+
+        public ActionResult PostList()
+        {
+            var res = dbObj.tbl_PostList.ToList();
+            return View(res);
+        }
+
+        public ActionResult Delete(int id)
+        {
+            var res = dbObj.tbl_PostList.Where(x => x.ID == id).First();
+            dbObj.tbl_PostList.Remove(res);
+            dbObj.SaveChanges();
+            var list = dbObj.tbl_PostList.ToList();
+
+            return View("PostList", list);
+        }
+    }
+}
---
+++ FA.JustBlogg/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FA.JustBlogg.Startup))]
+namespace FA.JustBlogg
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
